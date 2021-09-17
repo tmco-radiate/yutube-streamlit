afe555e0ef737a7c00c2024a1e7d63f7ce2675d6
kuni <tetsuji.kunitake@jp.ricoh.com>
2021-09-17 23:38:27 +0900

    1st commit

---
+++ PycharmProjects/pythonProject/main.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import time
+
+st.title('steramlit 超入門')
+
+st.write('プログレスバー表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+
+#text = st.text_input('あなたの趣味を教えて下さい')
+#condition = st.slider('あなたの今の調子は？',0,100,50)
+
+#'あなたの趣味は、',text
+#'コンディション:',condition
+
+
+#if st.checkbox('show image'):
+#    img = Image.open('sample.jpg')
+#    st.image(img, caption='Monkey',use_column_width=False)
+
+
+
